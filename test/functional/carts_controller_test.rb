93ac0d6ae65c99e7d8d1561a280e9a44a69d7545
venkatasubbaiah <venky.kandagaddala@gmail.com>
2013-04-17 12:56:37 +0530

    created a smarter cart

--- test/functional/carts_controller_test.rb
+++ test/functional/carts_controller_test.rb
@@ -18,7 +18,7 @@ class CartsControllerTest < ActionController::TestCase
 
   test "should create cart" do
     assert_difference('Cart.count') do
-      post :create, cart: {  }
+      post :create, cart: {}
     end
 
     assert_redirected_to cart_path(assigns(:cart))
@@ -35,15 +35,15 @@ class CartsControllerTest < ActionController::TestCase
   end
 
   test "should update cart" do
-    put :update, id: @cart, cart: {  }
+    put :update, id: @cart, cart: {}
     assert_redirected_to cart_path(assigns(:cart))
   end
 
   test "should destroy cart" do
     assert_difference('Cart.count', -1) do
+      session[:cart_id] = @cart.id
       delete :destroy, id: @cart
     end
-
-    assert_redirected_to carts_path
+    assert_redirected_to store_path
   end
 end
