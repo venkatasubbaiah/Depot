c978743e56d6bdd887e909d64adc74a0f2951ea4
venkatasubbaiah <venky.kandagaddala@gmail.com>
2013-04-18 14:02:56 +0530

    design smartcart using ajax

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def hidden_div_if(condition, attributes = {}, &block)
+    if condition
+      attributes["style"] = "display: true"
+    end
+    content_tag("div", attributes, &block)
+  end
 end
